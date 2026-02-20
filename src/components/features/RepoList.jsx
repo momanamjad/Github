22a3c41011b0fc0bacc571d962ccf512354a6c36
MOMAN AMJAD <momanamjad07@gmail.com>
2026-02-20 11:15:05 +0500

    changes

--- src/components/features/RepoList.jsx
+++ src/components/features/RepoList.jsx
@@ -12,6 +12,9 @@ const RepoList = ({ repos }) => {
               // to={`/${repo.owner.login}/${repo.name}`}
               className="text-[#0969DA] text-[22px] font-semibold   hover:underline"
             >
+
+
+              s
               {repo.name}
             </Link>
 
